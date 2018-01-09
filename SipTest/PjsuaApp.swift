33bd48e0da5d56d1b5e1fa471e3128094225d337
zuchie <pete.cui@butterflymx.com>
2018-01-09 03:06:40 -0800

    Video is working now.

---
+++ SipTest/PjsuaApp.swift
@@ -0,0 +1,15 @@
+//
+//  PjsuaApp.swift
+//  SipTest
+//
+//  Created by Zhe Cui on 1/8/18.
+//  Copyright © 2018 Zhe Cui. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PjsuaApp {
+    
+
+}
